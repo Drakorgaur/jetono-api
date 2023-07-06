66a9e3d5ff39f97e3378e942db03313cd915f7e3
pomo <mark.barzali@firma.seznam.cz>
2023-07-06 15:30:48 +0200

    feat: add operator route accepts flags

--- src/operator.go
+++ src/operator.go
@@ -3,7 +3,6 @@ package src
 import (
 	"github.com/labstack/echo/v4"
 	nsc "github.com/nats-io/nsc/cmd"
-	"github.com/spf13/cobra"
 )
 
 func init() {
@@ -20,16 +19,35 @@ func init() {
 // @Router			/operator [post]
 // @Summary		Add an operator
 // @Description	Add an operator to the store
-// @Param			name	formData	string				true	"Operator name"
-// @Success		200		{object}	SimpleJSONResponse	"Operator added"
-// @Failure		400		{object}	SimpleJSONResponse	"Bad request"
-// @Failure		500		{object}	string				"Internal error"
+// @Param			name					formData	string				true	"Operator name"
+// @Param			generate-signing-key	formData	bool				false	"generate a signing key with the operator"
+// @Param			sys						formData	bool				false	"generate system account with the operator (if specified will be signed with signing key)"
+// @Param			force					formData	bool				false	"on import, overwrite existing when already present"
+// @Param			start					formData	string				false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+// @Param			expiry					formData	string				false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+// @Success		200						{object}	SimpleJSONResponse	"Operator added"
+// @Failure		400						{object}	SimpleJSONResponse	"Bad request"
+// @Failure		500						{object}	string				"Internal error"
 func addOperator(c echo.Context) error {
-	var params nsc.AddOperatorParams // TODO: fill params by api body
+	var addCmd = lookupCommand(nsc.GetRootCmd(), "add")
+	var addOperatorCmd = lookupCommand(addCmd, "operator")
 
-	if err := nsc.RunStoreLessAction(&cobra.Command{}, []string{c.FormValue("name")}, &params); err != nil {
+	err := setFlagsIfInForm(addOperatorCmd, c.FormValue, []string{
+		// "url", is not supported yet.
+		"generate-signing-key",
+		"sys",
+		"force",
+		"start",
+		"expiry",
+	})
+	if err != nil {
 		return badRequest(c, err)
 	}
+
+	if err := addOperatorCmd.RunE(addOperatorCmd, []string{c.FormValue("name")}); err != nil {
+		return badRequest(c, err)
+	}
+
 	return c.JSON(200, &SimpleJSONResponse{
 		Status:  "200",
 		Message: "Operator added",
