bf615edd8ce4d8dd3795813e3d27590b83d0f8d0
pomo <mark.barzali@firma.seznam.cz>
2023-07-10 04:57:01 +0200

    feat: update account route
    HTTP PATCH method is added to update existing account

--- src/account.go
+++ src/account.go
@@ -14,6 +14,8 @@ func init() {
 	GetEchoRoot().GET("operator/:operator/"+module+"s", listAccounts)
 
 	GetEchoRoot().GET("operator/:operator/"+module+"/:name", describeAccount)
+
+	GetEchoRoot().PATCH("operator/:operator/"+module+"/:name", updateAccount)
 }
 
 // @Tags			Account
@@ -22,7 +24,6 @@ func init() {
 // @Description	Add an account with given operator to the store
 // @Param			name				formData	string				true	"Account name"
 // @Param			operator			path		string				true	"Operator name"
-//
 // @Param			response-ttl		formData	string				false	"the amount of time the default permission is valid (global) - [#ms(millis) | #s(econds) | m(inutes) | h(ours)] - Default is no time limit."
 // @Param			allow-pub-response	formData	string				false	"%s to limit how often a client can publish to reply subjects [with an optional count, --allow-pub-response=n] (global)"
 // @Param			allow-pub			formData	string				false	"add publish %s - comma separated list or option can be specified multiple times"
@@ -120,4 +121,78 @@ func describeAccount(c echo.Context) error {
 	}
 
 	return c.JSONBlob(200, body)
+}
+
+// @Tags			Account
+// @Router			/operator/{operator}/account/{name} [patch]
+// @Param			name		path	string	true	"Account name"
+// @Param			operator	path	string	true	"Operator name"
+// @Param			tag						formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
+// @Param			rm-tag					formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
+// @Param			conns					formData	string	false	"set maximum active connections for the account (-1 is unlimited)"
+// @Param			leaf-conns				formData	string	false	"set maximum active leaf node connections for the account (-1 is unlimited)"
+// @Param			data					formData	string	false	"set maximum data in bytes for the account (-1 is unlimited)"
+// @Param			exports					formData	string	false	"set maximum number of exports for the account (-1 is unlimited)"
+// @Param			imports					formData	string	false	"set maximum number of imports for the account (-1 is unlimited)"
+// @Param			payload					formData	string	false	"set maximum message payload in bytes for the account (-1 is unlimited)"
+// @Param			subscriptions			formData	string	false	"set maximum subscription for the account (-1 is unlimited)"
+// @Param			wildcard-exports		formData	string	false	"exports can contain wildcards"
+// @Param			disallow-bearer			formData	string	false	"require user jwt to not be bearer token"
+// @Param			rm-sk					formData	string	false	"remove signing key - comma separated list or option can be specified multiple times"
+// @Param			description				formData	string	false	"Description for this account"
+// @Param			info-url				formData	string	false	"Link for more info on this account"
+// @Param			js-tier					formData	string	false	"JetStream: replication tier (0 creates a configuration that applies to all assets) "
+// @Param			rm-js-tier				formData	string	false	"JetStream: remove replication limits for the specified tier (0 is the global tier) this flag is exclusive of all other js flags"
+// @Param			js-mem-storage			formData	string	false	"JetStream: set maximum memory storage in bytes for the account (-1 is unlimited / 0 disabled) (units: k/m/g/t kib/mib/gib/tib)"
+// @Param			js-disk-storage			formData	string	false	"JetStream: set maximum disk storage in bytes for the account (-1 is unlimited / 0 disabled) (units: k/m/g/t kib/mib/gib/tib)"
+// @Param			js-streams				formData	string	false	"JetStream: set maximum streams for the account (-1 is unlimited)"
+// @Param			js-consumer				formData	string	false	"JetStream: set maximum consumer for the account (-1 is unlimited)"
+// @Param			js-max-mem-stream		formData	string	false	"JetStream: set maximum size of a memory stream for the account (-1 is unlimited / 0 disabled) (units: k/m/g/t kib/mib/gib/tib)"
+// @Param			js-max-disk-stream		formData	string	false	"JetStream: set maximum size of a disk stream for the account (-1 is unlimited / 0 disabled) (units: k/m/g/t kib/mib/gib/tib)"
+// @Param			js-max-bytes-required	formData	string	false	"JetStream: set whether max stream is required when creating a stream"
+// @Param			js-max-ack-pending		formData	string	false	"JetStream: set number of maximum acks that can be pending for a consumer in the account"
+// @Param			name					formData	string	false	"account to edit"
+// @Param			js-disable				formData	string	false	"disables all JetStream limits in the account by deleting any limits"
+// @Summary		Updates an account
+// @Description	Updates an account and returns json with status ok if successful
+// @Success		200	{object}	SimpleJSONResponse	"Status ok"
+// @Failure		500	{object}	string				"Internal error"
+func updateAccount(c echo.Context) error {
+	var updateCmd = lookupCommand(nsc.GetRootCmd(), "edit")
+	var updateAccountCmd = lookupCommand(updateCmd, "account")
+
+	if err := nsc.GetConfig().SetOperator(c.Param("operator")); err != nil {
+		return badRequest(c, err)
+	}
+
+	err := setFlagsIfInForm(updateAccountCmd, c.FormValue, []string{
+		"start", "expiry", "tag", "rm-tag", "conns", "leaf-conns", "exports", "imports", "subscriptions",
+		"payload", "data", "wildcard-exports", "sk", "rm-sk", "description", "info-url", "response-ttl", "allow-pub-response",
+		"allow-pub-response", "allow-pub", "allow-pubsub", "allow-sub", "deny-pub", "deny-pubsub", "deny-sub",
+		"rm-response-perms", "rm", "max-responses", "disallow-bearer",
+		"js-tier",
+		"rm-js-tier",
+		"js-mem-storage",
+		"js-disk-storage",
+		"js-streams",
+		"js-consumer",
+		"js-max-mem-stream",
+		"js-max-disk-stream",
+		"js-max-bytes-required",
+		"js-max-ack-pending",
+		"js-disable",
+	})
+
+	if err != nil {
+		return badRequest(c, err)
+	}
+
+	if err := updateAccountCmd.RunE(updateAccountCmd, []string{c.Param("name")}); err != nil {
+		return badRequest(c, err)
+	}
+
+	return c.JSON(200, &SimpleJSONResponse{
+		Status:  "200",
+		Message: "Account updated",
+	})
 }
