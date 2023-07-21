2dd0ae6254bc43b2b7a2f94be21b806cb7e79b88
pomo <mark.barzali@firma.seznam.cz>
2023-07-21 19:32:43 +0200

    feat: reset middleware

--- src/adapter.go
+++ src/adapter.go
@@ -1,8 +1,29 @@
 package src
 
 import (
+	"github.com/labstack/echo/v4"
+	nsc "github.com/nats-io/nsc/cmd"
 	"github.com/spf13/cobra"
 )
+
+func init() {
+	resetMiddleware()
+}
+
+func resetMiddleware() {
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			resetGlobalVars()
+			return next(c)
+		}
+	})
+}
+
+// resetGlobalVars resets global variables from nsc package as one
+// command can set it up, which will affect other commands.
+func resetGlobalVars() {
+	nsc.KeyPathFlag = ""
+}
 
 func lookupCommand(root *cobra.Command, name string) *cobra.Command {
 	for _, e := range root.Commands() {
