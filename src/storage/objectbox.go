064f94ab32d8344551a887233b62ab42f6455c5b
pomo <mark.barzali@firma.seznam.cz>
2023-07-22 18:50:33 +0200

    fix: object box can update keys

--- src/storage/objectbox.go
+++ src/storage/objectbox.go
@@ -1,11 +1,14 @@
 package storage
 
 import (
-	"fmt"
 	"github.com/objectbox/objectbox-go/objectbox"
 )
 
 type ObjectBoxStore struct{}
+
+type ErrNotFound struct{}
+
+func (e ErrNotFound) Error() string { return "object(s) was not found" }
 
 func initObjectBox() (*objectbox.ObjectBox, error) {
 	objectBox, err := objectbox.NewBuilder().Model(ObjectBoxModel()).Build()
@@ -16,7 +19,7 @@ func initObjectBox() (*objectbox.ObjectBox, error) {
 	return objectBox, nil
 }
 
-func (s *ObjectBoxStore) Store(usm *AccountServerMap) error {
+func (s *ObjectBoxStore) Store(asm *AccountServerMap) error {
 	ob, err := initObjectBox()
 	if err != nil {
 		return err
@@ -25,19 +28,42 @@ func (s *ObjectBoxStore) Store(usm *AccountServerMap) error {
 
 	box := BoxForAccountServerMapB(ob)
 
-	// Create
-	_, err = box.Put(&AccountServerMapB{
-		AccountServerMap: *usm,
-		Uid:              GenerateUid(usm.Operator, usm.Account),
-	})
-	if err != nil {
-		return err
+	asmb, err := getAccountServerMapB(box, asm)
+	if _, ok := err.(ErrNotFound); ok {
+		_, err = box.Put(&AccountServerMapB{
+			AccountServerMap: *asm,
+			Uid:              GenerateUid(asm.Operator, asm.Account),
+		})
+		if err != nil {
+			return err
+		}
+	} else {
+		asmb.AccountServerMap = *asm
+		err = box.Update(asmb)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func (s *ObjectBoxStore) Read(usm *AccountServerMap) error {
+func getAccountServerMapB(box *AccountServerMapBBox, asm *AccountServerMap) (*AccountServerMapB, error) {
+	res, err := box.Query(AccountServerMapB_.Uid.Equals(GenerateUid(
+		asm.Operator, asm.Account,
+	), true)).Find()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(res) == 0 {
+		return nil, ErrNotFound{}
+	}
+
+	return res[0], nil
+}
+
+func (s *ObjectBoxStore) Read(asm *AccountServerMap) error {
 	ob, err := initObjectBox()
 	if err != nil {
 		return err
@@ -46,20 +72,12 @@ func (s *ObjectBoxStore) Read(usm *AccountServerMap) error {
 
 	box := BoxForAccountServerMapB(ob)
 
-	res, err := box.Query(AccountServerMapB_.Uid.Equals(GenerateUid(
-		usm.Operator, usm.Account,
-	), true)).Find()
+	asmb, err := getAccountServerMapB(box, asm)
 	if err != nil {
 		return err
 	}
 
-	if len(res) == 0 {
-		return fmt.Errorf("no result")
-	}
-
-	usmb := res[0]
-
-	usm.Servers = usmb.Servers
+	asm.Servers = asmb.Servers
 
 	return nil
 }
