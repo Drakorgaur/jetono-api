a65714cf34452d4a11cc7ade52e50456b91c6f23
pomo <mark.barzali@firma.seznam.cz>
2023-07-29 12:32:38 +0200

    docs: documentation for bind routes

--- src/operator.go
+++ src/operator.go
@@ -17,19 +17,19 @@ func init() {
 	GetEchoRoot().PATCH("operator/:name", updateOperator)
 }
 
-// @Tags			Operator
-// @Router			/operator [post]
-// @Summary		Add an operator
-// @Description	Add an operator to the store
-// @Param			name					formData	string				true	"Operator name"
-// @Param			generate-signing-key	formData	bool				false	"generate a signing key with the operator"
-// @Param			sys						formData	bool				false	"generate system account with the operator (if specified will be signed with signing key)"
-// @Param			force					formData	bool				false	"on import, overwrite existing when already present"
-// @Param			start					formData	string				false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Param			expiry					formData	string				false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Success		200						{object}	SimpleJSONResponse	"Operator added"
-// @Failure		400						{object}	SimpleJSONResponse	"Bad request"
-// @Failure		500						{object}	string				"Internal error"
+//	@Tags			Operator
+//	@Router			/operator [post]
+//	@Summary		Add an operator
+//	@Description	Add an operator to the store
+//	@Param			name					formData	string				true	"Operator name"
+//	@Param			generate-signing-key	formData	bool				false	"generate a signing key with the operator"
+//	@Param			sys						formData	bool				false	"generate system account with the operator (if specified will be signed with signing key)"
+//	@Param			force					formData	bool				false	"on import, overwrite existing when already present"
+//	@Param			start					formData	string				false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+//	@Param			expiry					formData	string				false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+//	@Success		200						{object}	SimpleJSONResponse	"Operator added"
+//	@Failure		400						{object}	SimpleJSONResponse	"Bad request"
+//	@Failure		500						{object}	string				"Internal error"
 func addOperator(c echo.Context) error {
 	var addCmd = lookupCommand(nsc.GetRootCmd(), "add")
 	var addOperatorCmd = lookupCommand(addCmd, "operator")
@@ -61,25 +61,25 @@ func addOperator(c echo.Context) error {
 	})
 }
 
-// @Tags			Operator
-// @Router			/operators [get]
-// @Summary		List operators
-// @Description	Returns json list of existing operators
-// @Success		200	{object}	[]string	"Operators list"
-// @Failure		500	{object}	string		"Internal error"
+//	@Tags			Operator
+//	@Router			/operators [get]
+//	@Summary		List operators
+//	@Description	Returns json list of existing operators
+//	@Success		200	{object}	[]string	"Operators list"
+//	@Failure		500	{object}	string		"Internal error"
 func listOperators(c echo.Context) error {
 	operators := nsc.GetConfig().ListOperators()
 
 	return c.JSON(200, map[string][]string{"operators": operators})
 }
 
-// @Tags			Operator
-// @Router			/operator/{name} [get]
-// @Param			name	path	string	true	"Operator name"
-// @Summary		Describes an operator
-// @Description	Returns json object with operator description
-// @Success		200	{object}	OperatorDescription	"Operator description"
-// @Failure		500	{object}	string				"Internal error"
+//	@Tags			Operator
+//	@Router			/operator/{name} [get]
+//	@Param			name	path	string	true	"Operator name"
+//	@Summary		Describes an operator
+//	@Description	Returns json object with operator description
+//	@Success		200	{object}	OperatorDescription	"Operator description"
+//	@Failure		500	{object}	string				"Internal error"
 func describeOperator(c echo.Context) error {
 	s, err := nsc.GetStoreForOperator(c.Param("name"))
 	if err != nil {
@@ -102,21 +102,21 @@ func describeOperator(c echo.Context) error {
 	)
 }
 
-// @Tags			Operator
-// @Router			/operator/{name} [patch]
-// @Param			name						path		string	true	"Operator name"
-// @Param			tag							formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
-// @Param			rm-tag						formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
-// @Param			account-jwt-server-url		formData	string	false	"set account jwt server url for nsc sync (only http/https/nats urls supported if updating with nsc)"
-// @Param			system-account				formData	string	false	"set system account by account by public key or name"
-// @Param			service-url					formData	string	false	"add an operator service url - comma separated list or option can be specified multiple times"
-// @Param			rm-service-url				formData	string	false	"remove an operator service url - comma separated list or option can be specified multiple times"
-// @Param			require-signing-keys		formData	bool    false	"require accounts/user to be signed with a signing key"
-// @Param			rm-account-jwt-server-url	formData	string	false	"clear account server url"
-// @Summary		Updates an operator
-// @Description	Updates an operator and returns json with status ok if successful
-// @Success		200	{object}	SimpleJSONResponse	"Status ok"
-// @Failure		500	{object}	string				"Internal error"
+//	@Tags			Operator
+//	@Router			/operator/{name} [patch]
+//	@Param			name						path		string	true	"Operator name"
+//	@Param			tag							formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
+//	@Param			rm-tag						formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
+//	@Param			account-jwt-server-url		formData	string	false	"set account jwt server url for nsc sync (only http/https/nats urls supported if updating with nsc)"
+//	@Param			system-account				formData	string	false	"set system account by account by public key or name"
+//	@Param			service-url					formData	string	false	"add an operator service url - comma separated list or option can be specified multiple times"
+//	@Param			rm-service-url				formData	string	false	"remove an operator service url - comma separated list or option can be specified multiple times"
+//	@Param			require-signing-keys		formData	bool	false	"require accounts/user to be signed with a signing key"
+//	@Param			rm-account-jwt-server-url	formData	string	false	"clear account server url"
+//	@Summary		Updates an operator
+//	@Description	Updates an operator and returns json with status ok if successful
+//	@Success		200	{object}	SimpleJSONResponse	"Status ok"
+//	@Failure		500	{object}	string				"Internal error"
 func updateOperator(c echo.Context) error {
 	var updateCmd = lookupCommand(nsc.GetRootCmd(), "edit")
 	var updateOperatorCmd = lookupCommand(updateCmd, "operator")
--- src/user.go
+++ src/user.go
@@ -26,16 +26,16 @@ func init() {
 	GetEchoRoot().PATCH("operator/:operator/account/:account/"+module+"/:name", updateUser)
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user [post]
-// @Summary		Add user
-// @Description	Add user with given operator and account to the store
-// @Param			name		formData	string				sstrue	"Username"
-// @Param			account		path		string				sstrue	"Account name"
-// @Param			operator	path		string				sstrue	"Operator name"
-// @Success		200			{object}	SimpleJSONResponse	"User added"
-// @Failure		400			{object}	SimpleJSONResponse	"Bad request"
-// @Failure		500			{object}	string				"Internal error"
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user [post]
+//	@Summary		Add user
+//	@Description	Add user with given operator and account to the store
+//	@Param			name		formData	string				sstrue	"Username"
+//	@Param			account		path		string				sstrue	"Account name"
+//	@Param			operator	path		string				sstrue	"Operator name"
+//	@Success		200			{object}	SimpleJSONResponse	"User added"
+//	@Failure		400			{object}	SimpleJSONResponse	"Bad request"
+//	@Failure		500			{object}	string				"Internal error"
 func addUser(c echo.Context) error {
 	cmd := nsc.CreateAddUserCmd()
 	nsc.GetConfig().Operator = c.Param("operator")
@@ -69,14 +69,14 @@ func addUser(c echo.Context) error {
 	})
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/users [get]
-// @Summary		List users
-// @Param			account		path	string	true	"Account name"
-// @Param			operator	path	string	true	"Operator name"
-// @Description	Returns json list of existing users for given operator's account
-// @Success		200	{object}	[]string	"List of users for given operator's account"
-// @Failure		500	{object}	string		"Internal error"
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/users [get]
+//	@Summary		List users
+//	@Param			account		path	string	true	"Account name"
+//	@Param			operator	path	string	true	"Operator name"
+//	@Description	Returns json list of existing users for given operator's account
+//	@Success		200	{object}	[]string	"List of users for given operator's account"
+//	@Failure		500	{object}	string		"Internal error"
 func listUsers(c echo.Context) error {
 	nsc.GetConfig().Operator = c.Param("operator")
 	nsc.GetConfig().Account = c.Param("account")
@@ -109,15 +109,15 @@ func listUsers(c echo.Context) error {
 	return c.JSON(200, map[string][]string{"users": users})
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user/{name} [get]
-// @Param			name		path	string	true	"Username"
-// @Param			account		path	string	true	"Account name"
-// @Param			operator	path	string	true	"Operator name"
-// @Summary		Describes user
-// @Description	Returns json object with user description
-// @Success		200	{object}	UserDescription	"Operator description"
-// @Failure		500	{object}	string			"Internal error"
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user/{name} [get]
+//	@Param			name		path	string	true	"Username"
+//	@Param			account		path	string	true	"Account name"
+//	@Param			operator	path	string	true	"Operator name"
+//	@Summary		Describes user
+//	@Description	Returns json object with user description
+//	@Success		200	{object}	UserDescription	"Operator description"
+//	@Failure		500	{object}	string			"Internal error"
 func describeUser(c echo.Context) error {
 	s, err := nsc.GetStoreForOperator(c.Param("operator"))
 	if err != nil {
@@ -166,16 +166,16 @@ func GetUserCreds(operator string, account string, user string) ([]byte, error) 
 	return d, nil
 }
 
-// @Tags		User
-// @Router		/creds/operator/{operator}/account/{account}/user/{name} [get]
-// @Summary	Generate user credentials
-// @Param		name		path	string	true	"Username"
-// @Param		account		path	string	true	"Account name"
-// @Param		operator	path	string	true	"Operator name"
-// @Description
-// @Success	200	{object}	map[string]string	"Operators list"
-// @Success	404	{object}	map[string]string	"User was not found"
-// @Failure	500	{object}	string				"Internal error"
+//	@Tags		User
+//	@Router		/creds/operator/{operator}/account/{account}/user/{name} [get]
+//	@Summary	Generate user credentials
+//	@Param		name		path	string	true	"Username"
+//	@Param		account		path	string	true	"Account name"
+//	@Param		operator	path	string	true	"Operator name"
+//	@Description
+//	@Success	200	{object}	map[string]string	"Operators list"
+//	@Success	404	{object}	map[string]string	"User was not found"
+//	@Failure	500	{object}	string				"Internal error"
 func generateUser(c echo.Context) error {
 	d, err := GetUserCreds(c.Param("operator"), c.Param("account"), c.Param("user"))
 	if err != nil {
@@ -185,15 +185,15 @@ func generateUser(c echo.Context) error {
 	return c.JSON(200, map[string]string{"creds": string(d)})
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user/{name} [delete]
-// @Param			name		path	string	true	"Username"
-// @Param			account		path	string	true	"Account name"
-// @Param			operator	path	string	true	"Operator name"
-// @Summary		Revokes a user
-// @Description	Revokes a user
-// @Success		200	{object}	map[string]string	"Operator description"
-// @Failure		500	{object}	string				"Internal error"
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user/{name} [delete]
+//	@Param			name		path	string	true	"Username"
+//	@Param			account		path	string	true	"Account name"
+//	@Param			operator	path	string	true	"Operator name"
+//	@Summary		Revokes a user
+//	@Description	Revokes a user
+//	@Success		200	{object}	map[string]string	"Operator description"
+//	@Failure		500	{object}	string				"Internal error"
 func revokeUser(c echo.Context) error {
 	var revokeCmd = lookupCommand(nsc.GetRootCmd(), "revocations")
 	var operatorCmd = lookupCommand(revokeCmd, "add-user")
@@ -215,40 +215,40 @@ func revokeUser(c echo.Context) error {
 	return c.JSON(200, map[string]string{"status": "ok"})
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user/{name} [patch]
-// @Param			name				path		string	true	"Username"
-// @Param			account				path		string	true	"Account name"
-// @Param			operator			path		string	true	"Operator name"
-// @Param			tag					formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
-// @Param			rm-tag				formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
-// @Param			start				formData	string	false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Param			expiry				formData	string	false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Param			time				formData	string	false	"`add start-end time range of the form "%s-%s" (option can be specified multiple times)`, timeFormat, timeFormat))"
-// @Param			rm-time				formData	string	false	"`remove start-end time by start time "%s" (option can be specified multiple times)`, timeFormat))"
-// @Param			locale				formData	string	false	"set the locale with which time values are interpreted")
-// @Param			source-network		formData	string	false	"add source network for connection - comma separated list or option can be specified multiple times")
-// @Param			rm-source-network	formData	string	false	"remove source network for connection - comma separated list or option can be specified multiple times")
-// @Param			conn-type			formData	string	false	"set	allowed	connection	types:	%s	%s	%s	%s	%s	%s	-	comma	separated	list	or	option	can	be	specified	multiple	times, jwt.ConnectionTypeLeafnode, jwt.ConnectionTypeMqtt, jwt.ConnectionTypeStandard, jwt.ConnectionTypeWebsocket, jwt.ConnectionTypeLeafnodeWS, jwt.ConnectionTypeMqttWS))"
-// @Param			rm-conn-type		formData	string	false	"remove connection types - comma separated list or option can be specified multiple times")
-// @Param			subs				formData	string	false	"set maximum number of subscriptions (-1 is unlimited)")
-// @Param			data				formData	string	false	"set maximum data in bytes for the user (-1 is unlimited)")
-// @Param			payload				formData	string	false	"set maximum message payload in bytes for the account (-1 is unlimited)")
-// @Param			bearer				formData	bool 	false	"no connect challenge required for user")
-// @Param			response-ttl		formData	string	false	"the amount of stime sthe s%s sis svalid s(global) - s[#ms(millis) | #s(econds) | m(inutes) | h(ours)] - sDefault sis no time limit., typeName))"
-// @Param			allow-pub-response	formData	string	false	"%s to slimit how soften sa sclient scan spublish sto sreply ssubjects	[with an optional count, --allow-pub-response=n] (global), typeName))"
-// @Param			allow-pub			formData	string	false	"add publish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple stimes, typeName))"
-// @Param			allow-pubsub		formData	string	false	"add publish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified	multiple stimes, typeName))"
-// @Param			allow-sub			formData	string	false	"add subscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple stimes, typeName))"
-// @Param			deny-pub			formData	string	false	"add deny spublish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
-// @Param			deny-pubsub			formData	string	false	"add deny spublish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple times, typeName))"
-// @Param			deny-sub			formData	string	false	"add deny ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
-// @Param			rm-response-perms	formData	string	false	"remove sresponse ssettings sfrom s%s, typeName))"
-// @Param			rm					formData	string	false	"remove spublish/subscribe sand sdeny s%s - comma separated	list or	option can be specified multiple times, typeName))"
-// @Summary		Updates an user
-// @Description	Updates an user and returns json with status ok if successful
-// @Success		200	{object}	SimpleJSONResponse	"Status ok"
-// @Failure		500	{object}	string				"Internal error"
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user/{name} [patch]
+//	@Param			name				path		string	true	"Username"
+//	@Param			account				path		string	true	"Account name"
+//	@Param			operator			path		string	true	"Operator name"
+//	@Param			tag					formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
+//	@Param			rm-tag				formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
+//	@Param			start				formData	string	false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+//	@Param			expiry				formData	string	false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
+//	@Param			time				formData	string	false	"`add start-end time range of the form "%s-%s" (option can be specified multiple times)`, timeFormat, timeFormat))"
+//	@Param			rm-time				formData	string	false	"`remove start-end time by start time "%s" (option can be specified multiple times)`, timeFormat))"
+//	@Param			locale				formData	string	false	"set the locale with which time values are interpreted")
+//	@Param			source-network		formData	string	false	"add source network for connection - comma separated list or option can be specified multiple times")
+//	@Param			rm-source-network	formData	string	false	"remove source network for connection - comma separated list or option can be specified multiple times")
+//	@Param			conn-type			formData	string	false	"set	allowed	connection	types:	%s	%s	%s	%s	%s	%s	-	comma	separated	list	or	option	can	be	specified	multiple	times, jwt.ConnectionTypeLeafnode, jwt.ConnectionTypeMqtt, jwt.ConnectionTypeStandard, jwt.ConnectionTypeWebsocket, jwt.ConnectionTypeLeafnodeWS, jwt.ConnectionTypeMqttWS))"
+//	@Param			rm-conn-type		formData	string	false	"remove connection types - comma separated list or option can be specified multiple times")
+//	@Param			subs				formData	string	false	"set maximum number of subscriptions (-1 is unlimited)")
+//	@Param			data				formData	string	false	"set maximum data in bytes for the user (-1 is unlimited)")
+//	@Param			payload				formData	string	false	"set maximum message payload in bytes for the account (-1 is unlimited)")
+//	@Param			bearer				formData	bool	false	"no connect challenge required for user")
+//	@Param			response-ttl		formData	string	false	"the amount of stime sthe s%s sis svalid s(global) - s[#ms(millis) | #s(econds) | m(inutes) | h(ours)] - sDefault sis no time limit., typeName))"
+//	@Param			allow-pub-response	formData	string	false	"%s to slimit how soften sa sclient scan spublish sto sreply ssubjects	[with an optional count, --allow-pub-response=n] (global), typeName))"
+//	@Param			allow-pub			formData	string	false	"add publish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple stimes, typeName))"
+//	@Param			allow-pubsub		formData	string	false	"add publish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified	multiple stimes, typeName))"
+//	@Param			allow-sub			formData	string	false	"add subscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple stimes, typeName))"
+//	@Param			deny-pub			formData	string	false	"add deny spublish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
+//	@Param			deny-pubsub			formData	string	false	"add deny spublish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple times, typeName))"
+//	@Param			deny-sub			formData	string	false	"add deny ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
+//	@Param			rm-response-perms	formData	string	false	"remove sresponse ssettings sfrom s%s, typeName))"
+//	@Param			rm					formData	string	false	"remove spublish/subscribe sand sdeny s%s - comma separated	list or	option can be specified multiple times, typeName))"
+//	@Summary		Updates an user
+//	@Description	Updates an user and returns json with status ok if successful
+//	@Success		200	{object}	SimpleJSONResponse	"Status ok"
+//	@Failure		500	{object}	string				"Internal error"
 func updateUser(c echo.Context) error {
 	var updateCmd = lookupCommand(nsc.GetRootCmd(), "edit")
 	var updateUserCmd = lookupCommand(updateCmd, "user")
