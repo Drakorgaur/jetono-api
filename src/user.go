012764e247036d6bcccca420084130d89fdcd248
pomo <mark.barzali@firma.seznam.cz>
2023-08-13 15:13:03 +0200

    feat: json'ification `user.go`

--- src/user.go
+++ src/user.go
@@ -32,32 +32,32 @@ func init() {
 	GetEchoRoot().GET("streams", getUserStreams)
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user [post]
-// @Summary		Add user
-// @Description	Add user with given operator and account to the store
-// @Param			name		formData	string				sstrue	"Username"
-// @Param			account		path		string				sstrue	"Account name"
-// @Param			operator	path		string				sstrue	"Operator name"
-// @Success		200			{object}	SimpleJSONResponse	"User added"
-// @Failure		400			{object}	SimpleJSONResponse	"Bad request"
-// @Failure		500			{object}	string				"Internal error"
+type addUserForm struct {
+	Name    string `json:"name"`
+	Account string `json:"account"`
+}
+
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user [post]
+//	@Summary		Add user
+//	@Description	Add user with given operator and account to the store
+//	@Param			json		body		addUserForm			true	"json"
+//	@Param			account		path		string				true	"Account name"
+//	@Param			operator	path		string				true	"Operator name"
+//	@Success		200			{object}	SimpleJSONResponse	"User added"
+//	@Failure		400			{object}	SimpleJSONResponse	"Bad request"
+//	@Failure		500			{object}	string				"Internal error"
 func addUser(c echo.Context) error {
 	cmd := nsc.CreateAddUserCmd()
-	nsc.GetConfig().Operator = c.Param("operator")
-	nsc.GetConfig().Account = c.Param("account")
-
-	if err, flag := raiseForRequiredFlags(c.FormValue, "name"); err != nil {
-		return c.JSON(400, map[string]string{"code": "400", "message": "required form data no filled", "field": flag})
+	if err := nsc.GetConfig().SetOperator(c.Param("operator")); err != nil {
+		return err
+	}
+	if err := nsc.GetConfig().SetAccount(c.Param("account")); err != nil {
+		return err
 	}
 
-	err := setFlagsIfInForm(cmd, c.FormValue, []string{
-		// "url", is not supported yet.
-		"name",
-		"tag",
-		"start",
-		"expiry",
-	})
+	s := &addUserForm{}
+	err := setFlagsIfInJson(cmd, s, c)
 	if err != nil {
 		return badRequest(c, err)
 	}
@@ -66,9 +66,10 @@ func addUser(c echo.Context) error {
 		return badRequest(c, err)
 	}
 
-	if err := cmd.RunE(cmd, []string{c.FormValue("name")}); err != nil {
+	if err := cmd.RunE(cmd, []string{s.Name}); err != nil {
 		return badRequest(c, err)
 	}
+
 	return c.JSON(200, &SimpleJSONResponse{
 		Status:  "200",
 		Message: "User added",
@@ -221,40 +222,44 @@ func revokeUser(c echo.Context) error {
 	return c.JSON(200, map[string]string{"status": "ok"})
 }
 
-// @Tags			User
-// @Router			/operator/{operator}/account/{account}/user/{name} [patch]
-// @Param			name				path		string	true	"Username"
-// @Param			account				path		string	true	"Account name"
-// @Param			operator			path		string	true	"Operator name"
-// @Param			tag					formData	string	false	"add tags for user - comma separated list or option can be specified multiple times"
-// @Param			rm-tag				formData	string	false	"remove tag - comma separated list or option can be specified multiple times"
-// @Param			start				formData	string	false	"valid from ('0' is always, '3d' is three days) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Param			expiry				formData	string	false	"valid until ('0' is always, '2M' is two months) - yyyy-mm-dd, #m(inutes), #h(ours), #d(ays), #w(eeks), #M(onths), #y(ears)"
-// @Param			time				formData	string	false	"`add start-end time range of the form "%s-%s" (option can be specified multiple times)`, timeFormat, timeFormat))"
-// @Param			rm-time				formData	string	false	"`remove start-end time by start time "%s" (option can be specified multiple times)`, timeFormat))"
-// @Param			locale				formData	string	false	"set the locale with which time values are interpreted")
-// @Param			source-network		formData	string	false	"add source network for connection - comma separated list or option can be specified multiple times")
-// @Param			rm-source-network	formData	string	false	"remove source network for connection - comma separated list or option can be specified multiple times")
-// @Param			conn-type			formData	string	false	"set	allowed	connection	types:	%s	%s	%s	%s	%s	%s	-	comma	separated	list	or	option	can	be	specified	multiple	times, jwt.ConnectionTypeLeafnode, jwt.ConnectionTypeMqtt, jwt.ConnectionTypeStandard, jwt.ConnectionTypeWebsocket, jwt.ConnectionTypeLeafnodeWS, jwt.ConnectionTypeMqttWS))"
-// @Param			rm-conn-type		formData	string	false	"remove connection types - comma separated list or option can be specified multiple times")
-// @Param			subs				formData	string	false	"set maximum number of subscriptions (-1 is unlimited)")
-// @Param			data				formData	string	false	"set maximum data in bytes for the user (-1 is unlimited)")
-// @Param			payload				formData	string	false	"set maximum message payload in bytes for the account (-1 is unlimited)")
-// @Param			bearer				formData	bool	false	"no connect challenge required for user")
-// @Param			response-ttl		formData	string	false	"the amount of stime sthe s%s sis svalid s(global) - s[#ms(millis) | #s(econds) | m(inutes) | h(ours)] - sDefault sis no time limit., typeName))"
-// @Param			allow-pub-response	formData	string	false	"%s to slimit how soften sa sclient scan spublish sto sreply ssubjects	[with an optional count, --allow-pub-response=n] (global), typeName))"
-// @Param			allow-pub			formData	string	false	"add publish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple stimes, typeName))"
-// @Param			allow-pubsub		formData	string	false	"add publish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified	multiple stimes, typeName))"
-// @Param			allow-sub			formData	string	false	"add subscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple stimes, typeName))"
-// @Param			deny-pub			formData	string	false	"add deny spublish s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
-// @Param			deny-pubsub			formData	string	false	"add deny spublish sand ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified multiple times, typeName))"
-// @Param			deny-sub			formData	string	false	"add deny ssubscribe s%s s- scomma sseparated slist sor soption scan sbe sspecified smultiple	times, typeName))"
-// @Param			rm-response-perms	formData	string	false	"remove sresponse ssettings sfrom s%s, typeName))"
-// @Param			rm					formData	string	false	"remove spublish/subscribe sand sdeny s%s - comma separated	list or	option can be specified multiple times, typeName))"
-// @Summary		Updates an user
-// @Description	Updates an user and returns json with status ok if successful
-// @Success		200	{object}	SimpleJSONResponse	"Status ok"
-// @Failure		500	{object}	string				"Internal error"
+type updateUserForm struct {
+	Tag              string `json:"tag,omitempty"`
+	RmTag            string `json:"rm_tag,omitempty"`
+	Start            string `json:"start,omitempty"`
+	Expiry           string `json:"expiry,omitempty"`
+	Time             string `json:"time,omitempty"`
+	RmTime           string `json:"rm_time,omitempty"`
+	Locale           string `json:"locale,omitempty"`
+	SourceNetwork    string `json:"source_network,omitempty"`
+	RmSourceNetwork  string `json:"rm_source_network,omitempty"`
+	ConnType         string `json:"conn_type,omitempty"`
+	RmConnType       string `json:"rm_conn_type,omitempty"`
+	Subs             string `json:"subs,omitempty"`
+	Data             string `json:"data,omitempty"`
+	Payload          string `json:"payload,omitempty"`
+	Bearer           bool   `json:"bearer,omitempty"`
+	ResponseTTL      string `json:"response_ttl,omitempty"`
+	AllowPubResponse string `json:"allow_pub_response,omitempty"`
+	AllowPub         string `json:"allow_pub,omitempty"`
+	AllowPubSub      string `json:"allow_pubsub,omitempty"`
+	AllowSub         string `json:"allow_sub,omitempty"`
+	DenyPub          string `json:"deny_pub,omitempty"`
+	DenyPubSub       string `json:"deny_pubsub,omitempty"`
+	DenySub          string `json:"deny_sub,omitempty"`
+	RmResponsePerms  string `json:"rm_response_perms,omitempty"`
+	Rm               string `json:"rm,omitempty"`
+}
+
+//	@Tags			User
+//	@Router			/operator/{operator}/account/{account}/user/{name} [patch]
+//	@Param			name		path	string			true	"Username"
+//	@Param			account		path	string			true	"Account name"
+//	@Param			operator	path	string			true	"Operator name"
+//	@Param			json		body	updateUserForm	true	"add tags for user - comma separated list or option can be specified multiple times"
+//	@Summary		Updates an user
+//	@Description	Updates an user and returns json with status ok if successful
+//	@Success		200	{object}	SimpleJSONResponse	"Status ok"
+//	@Failure		500	{object}	string				"Internal error"
 func updateUser(c echo.Context) error {
 	var updateCmd = lookupCommand(nsc.GetRootCmd(), "edit")
 	var updateUserCmd = lookupCommand(updateCmd, "user")
@@ -269,12 +274,7 @@ func updateUser(c echo.Context) error {
 		return badRequest(c, err)
 	}
 
-	err := setFlagsIfInForm(updateUserCmd, c.FormValue, []string{
-		"start", "expiry", "rm", "allow-pub", "allow-sub", "allow-pubsub",
-		"deny-pub", "deny-sub", "deny-pubsub", "tag", "rm-tag", "source-network", "rm-source-network", "payload",
-		"rm-response-perms", "max-responses", "response-ttl", "allow-pub-response", "bearer", "rm-time", "time", "conn-type",
-		"rm-conn-type", "subs", "data",
-	})
+	err := setFlagsIfInJson(updateUserCmd, &updateUserForm{}, c)
 	if err != nil {
 		return badRequest(c, err)
 	}
